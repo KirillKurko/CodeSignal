8fe26da8064c82cc3ca0d1ffa79900c71615df5a
Кирилл Курко <kirillkurko2609@gmail.com>
2020-06-16 20:05:50 +0300

    Initial Commit

---
+++ CodeSignal/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CodeSignal
+//
+//  Created by Кирилл Курко on 6/16/20.
+//  Copyright © 2020 Николай Курко. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
